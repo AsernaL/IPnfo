9e0007cc4d0c938a0a98e79948a8861ac47d00da
BerkeSwz <78283095+AsernaL@users.noreply.github.com>
2021-01-31 15:56:36 +0300

    Add files via upload

---
+++ iptrack.py
@@ -0,0 +1,61 @@
+import requests
+import json
+from pip._vendor.colorama import Fore
+
+print(Fore.MAGENTA+'      ▄▄▄        ██████ ▓█████  ██▀███   ███▄    █  ▄▄▄       ██▓     ')
+print(Fore.MAGENTA+'     ▒████▄    ▒██    ▒ ▓█   ▀ ▓██ ▒ ██▒ ██ ▀█   █ ▒████▄    ▓██▒     ')
+print(Fore.MAGENTA+'     ▒██  ▀█▄  ░ ▓██▄   ▒███   ▓██ ░▄█ ▒▓██  ▀█ ██▒▒██  ▀█▄  ▒██░     ')
+print(Fore.MAGENTA+'     ░██▄▄▄▄██   ▒   ██▒▒▓█  ▄ ▒██▀▀█▄  ▓██▒  ▐▌██▒░██▄▄▄▄██ ▒██░     ')
+print(Fore.MAGENTA+'      ▓█   ▓██▒▒██████▒▒░▒████▒░██▓ ▒██▒▒██░   ▓██░ ▓█   ▓██▒░██████▒ ')
+print(Fore.MAGENTA+'      ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░░░ ▒░ ░░ ▒▓ ░▒▓░░ ▒░   ▒ ▒  ▒▒   ▓▒█░░ ▒░▓  ░ ')
+print(Fore.MAGENTA+'       ▒   ▒▒ ░░ ░▒  ░ ░ ░ ░  ░  ░▒ ░ ▒░░ ░░   ░ ▒░  ▒   ▒▒ ░░ ░ ▒  ░ ')
+print(Fore.MAGENTA+'       ░   ▒   ░  ░  ░     ░     ░░   ░    ░   ░ ░   ░   ▒     ░ ░    ')
+print(Fore.MAGENTA+'           ░  ░      ░     ░  ░   ░              ░       ░  ░    ░  ░ ')
+print(Fore.MAGENTA+'                                                                      ')
+print(Fore.MAGENTA+'      ▄▄▄▄   ▓█████  ██▀███   ██ ▄█▀▓█████   ██████  █     █░▒███████▒')
+print(Fore.MAGENTA+'     ▓█████▄ ▓█   ▀ ▓██ ▒ ██▒ ██▄█▒ ▓█   ▀ ▒██    ▒ ▓█░ █ ░█░▒ ▒ ▒ ▄▀░')
+print(Fore.MAGENTA+'     ▒██▒ ▄██▒███   ▓██ ░▄█ ▒▓███▄░ ▒███   ░ ▓██▄   ▒█░ █ ░█ ░ ▒ ▄▀▒░   ')
+print(Fore.MAGENTA+'     ▒██░█▀  ▒▓█  ▄ ▒██▀▀█▄  ▓██ █▄ ▒▓█  ▄   ▒   ██▒░█░ █ ░█   ▄▀▒   ░')
+print(Fore.MAGENTA+'     ░▓█  ▀█▓░▒████▒░██▓ ▒██▒▒██▒ █▄░▒████▒▒██████▒▒░░██▒██▓ ▒███████▒')
+print(Fore.MAGENTA+'     ░▒▓███▀▒░░ ▒░ ░░ ▒▓ ░▒▓░▒ ▒▒ ▓▒░░ ▒░ ░▒ ▒▓▒ ▒ ░░ ▓░▒ ▒  ░▒▒ ▓░▒░▒')
+print(Fore.MAGENTA+'     ▒░▒   ░  ░ ░  ░  ░▒ ░ ▒░░ ░▒ ▒░ ░ ░  ░░ ░▒  ░ ░  ▒ ░ ░  ░░▒ ▒ ░ ▒')
+print(Fore.MAGENTA+'      ░    ░    ░     ░░   ░ ░ ░░ ░    ░   ░  ░  ░    ░   ░  ░ ░ ░ ░ ░')
+print(Fore.MAGENTA+'      ░         ░  ░   ░     ░  ░      ░  ░      ░      ░      ░ ░    ')
+print(Fore.MAGENTA+'           ░                                                 ░        ')
+
+print(Fore.YELLOW+'İnstagram = @Berkeswz')
+print(Fore.YELLOW+'Team = Black Cyber Team')
+
+ip_address = input(Fore.MAGENTA+'Ip/Network Gateway---->')
+
+response = requests.get(f'http://ip-api.com/json/{ip_address}').content
+
+data = json.loads(response)
+
+print(f"""
+IP = {data['query']}
+Country = {data['country']}
+City = {data['city']}
+ZIP = {data['zip']}
+ISP = {data['isp']}
+Lon = {data['lon']}
+Lat = {data['lat']}
+Location = {data['lat']},{data['lon']}
+Countrycode = {data['countryCode']}
+Region = {data['region']} 
+Timezone = {data['timezone']}
+As = {data['as']}
+""")
+
+print(Fore.MAGENTA+"""
+Ty For Using AsernaL ? ip Tracker
+This tool made by Berkeswz """)
+
+print('Follow on instagram')
+
+print(Fore.YELLOW+""" 
+@berkeswz
+@qwenzyreal
+""")
+
+print(Fore.RED+'////Black Cyber Team////')
