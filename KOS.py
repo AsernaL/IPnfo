672f8f466d23d6da896828a2b78e0569aabfd463
L4ncelot <78283095+KnightL4ncelot@users.noreply.github.com>
2021-03-07 14:50:43 +0300

    Python3

--- KOS.py
+++ KOS.py
@@ -1,6 +1,9 @@
 import requests
 import json
 from pip._vendor.colorama import Fore
+import subprocess
+
+subprocess.call('clear',shell=True)
 
 baner = ('''
   _   _  _                  _       _   
@@ -16,13 +19,14 @@ print(Fore.LIGHTBLUE_EX+baner)
 print(Fore.BLUE+'İnstagram = @KnightL4ncelot')
 print(Fore.CYAN+'KnihtsOfShadow #Anonymous')
 
-ip_address = input(Fore.CYAN+'Ip/Network Gateway---->')
+ip_address = input(Fore.CYAN+" ┌─/"+Fore.LIGHTBLUE_EX+"L4ncelot-Tools-IpTracking"+Fore.CYAN+"""/
+ └──╼ """+Fore.LIGHTBLUE_EX+"=> ")
 
 response = requests.get(f'http://ip-api.com/json/{ip_address}').content
 
 data = json.loads(response)
 
-print(Fore.WHITE+f"""
+print(Fore.CYAN+f"""
 IP = {data['query']}
 Country = {data['country']}
 City = {data['city']}
@@ -43,4 +47,4 @@ This tool made by L4ncelot """)
 
 
 
-print(Fore.WHITE+'KnightsOfShadow')
+print(Fore.CYAN+'KnightsOfShadow')
